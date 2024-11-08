384dff1c5cdf870b8eab162a66029b6df7d1f1f7
ShaolinRat <csprowalps@gmail.com>
2024-11-08 16:03:49 -0500

    adding files

---
+++ LogicalOperatorsDemo.java
@@ -0,0 +1,18 @@
+package com.glab_303_2_1;
+
+public class LogicalOperatorsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     boolean x = true;
+	       boolean y = false;
+	       System.out.println("x & y : " + (x & y));
+	       System.out.println("x && y : " + (x && y));
+	       System.out.println("x | y : " + (x | y));
+	       System.out.println("x || y: " + (x || y));
+	       System.out.println("x ^ y : " + (x ^ y));
+	       System.out.println("!x : " + (!x));
+
+	}
+
+}
